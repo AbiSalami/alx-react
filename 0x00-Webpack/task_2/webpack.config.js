3aa3691cf3d32bb65b20ccb882183eecafb6374b
AbiSalami <bimwealth215@gmail.com>
2024-08-22 07:51:26 +0000

    added image

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,10 +1,32 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  mode: 'production',
   entry: './js/dashboard_main.js',
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
+    publicPath: '/public'
   },
+  mode: 'production',
+  module: {
+	rules: [
+		{
+			test: /\.css$/i,
+			use: ["css-loader", "style-loader"],
+		},
+		{
+			test: /\.(gif|png|jpe?g|svg)$/i,
+			use: [
+				"file-loader",
+				{
+					loader: "image-webpack-loader",
+					options: {
+							bypassingOnDebug: true,
+							disable: true,
+					},
+				},
+			],
+		},
+	],
+},
 };
