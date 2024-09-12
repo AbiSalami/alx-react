b3509e4f24972100b92b2d38514dca6477a37ad8
AbiSalami <bimwealth215@gmail.com>
2024-09-12 12:15:35 -0700

    task0

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,16 +1,5 @@
-const { fromJS } = require('immutable');
+import { fronJS } from 'immutable';
 
-function getImmutableObject(object) {
-  return fromJS(object);
+export default function getImmutableObject(object) {
+    return fronJS(object);
 }
-
-// Example usage:
-const obj = {
-  fear: true,
-  smell: -1033575916.9145899,
-  wall: false,
-  thing: -914767132
-};
-
-const immutableObj = getImmutableObject(obj);
-console.log(immutableObj);
