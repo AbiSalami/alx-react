60ad49252a744bd6bef05f076d7221c8f6073e52
AbiSalami <bimwealth215@gmail.com>
2024-09-12 18:02:41 -0700

    task2

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,10 +1,7 @@
+import { fromJS } from 'immutable';
+
 export default function accessImmutableObject(object, array) {
-  let currentValue = object;
-  for (let i = 0; i < array.length; i++) {
-    if (!currentValue || currentValue[array[i]] === undefined) {
-      return undefined;
-    }
-    currentValue = currentValue[array[i]];
-  }
-  return currentValue;
+  const mappedObj = fromJS(object);
+
+  return mappedObj.getIn(array, undefined);
 }
